7e46382a5e11cbbbaefe3ca281ac3c5710fa001e
Jonathan Xavier <jonathan.xavier@gmail.com>
2014-09-30 10:18:15 -0300

    Default route revision.

--- bat/192.168.1.254.bat
+++ bat/192.168.1.254.bat
@@ -1,1 +1,1 @@
-netsh interface ipv4 set address name="Local Area Connection" static 192.168.1.254 255.255.255.0 192.168.1.1 1
+netsh interface ipv4 set address name="Local Area Connection" static 192.168.1.254 255.255.255.0
